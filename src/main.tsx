790e06cff0b5ecfa4ad2810d8348f30b32bec3eb
Outrageous-Yak <f5tyzzqpdw@privaterelay.appleid.com>
2026-01-14 18:10:03 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./ui/appBase.css";
 import App from "./ui/app";
 
 const rootEl = document.getElementById("app");
-if (!rootEl) {
-  throw new Error("Missing #app element");
-}
+if (!rootEl) throw new Error("Missing #app element");
 
 ReactDOM.createRoot(rootEl).render(
   <React.StrictMode>
