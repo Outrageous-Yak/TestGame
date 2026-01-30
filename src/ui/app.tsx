8a1795474c9513e3ce6fba70f297d3cda1d9128f
Outrageous-Yak <f5tyzzqpdw@privaterelay.appleid.com>
2026-01-30 09:52:10 +0100

    Update app.tsx

--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -3282,7 +3282,7 @@ const isOffset = cols === 6;
                 const ns = normalizeRowShift(rawShift, cols);
                 const shift = ns.visual;
 
-                const tx = isOffset ? "calc(var(--hexStepX) / 4)" : "0px";
+                const tx = isOffset ? "calc(var(--hexStepX) / 5)" : "0px";
 
                 return (
                   <div
