f120d1f8f52f0e6374ae557cdf859494c6c3f2c0
Outrageous-Yak <f5tyzzqpdw@privaterelay.appleid.com>
2026-02-16 10:45:38 +0100

    Update app.tsx

--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -4782,9 +4782,9 @@ return (
 
       {/* RIGHT: big die + info + controls */}
       <div className="encounterRight">
-        <div className="encounterCard riskCard">
+       
         <div className={"dice3d encounterDice " + (diceRolling ? "rolling" : "")}>
-            
+            <div className="encounterCard riskCard"> 
           <div
             className="cube"
             style={{
